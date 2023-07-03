3a255f6807777bd34a568e4679f2298bce0bab60
sergeymaksimov <sergey.maksimov-c@therapybrands.com>
2023-07-03 16:17:10 +0200

    first version

---
+++ src/extension.js
@@ -0,0 +1,31 @@
+const vscode = require('vscode');
+const fs = require('fs');
+
+const pluginName = 'VS-code-plugin-ex';
+
+function activate(context) {
+    const convert = vscode.commands.registerTextEditorCommand(pluginName + '.convert', (textEditor) => {
+        const options = vscode.workspace.getConfiguration(pluginName);
+  
+        const text = textEditor.document.getText();
+        text = text.replaceAll("const ", "const test-");
+        const filePath = textEditor.document.fileName;
+        var newFilePath=filePath.substring(0, filePath.lastIndexOf(".")) + "-test." + filePath.substring(filePath.lastIndexOf(".")+1);
+        fs.writeFile(newFilePath, text, (err) => {
+          if (err) {
+            return vscode.window.showErrorMessage('Failed to create testFile!');
+          }
+          vscode.window.showInformationMessage('testFile created');
+        });
+    });
+  
+    context.subscriptions.push(convert);
+  }
+  
+exports.activate = activate;
+
+function deactivate() {
+  // Code...
+}
+
+exports.deactivate = deactivate;
